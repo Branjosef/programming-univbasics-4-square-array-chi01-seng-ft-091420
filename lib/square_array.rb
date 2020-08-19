eee76741bb6791bf1c33cd4cdf9c57a2537666b1
Brandon Fonnemann <branjosef@gmail.com>
2020-08-19 17:58:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
   # your code here
   square_array = []
+  counter = 0
   while array.length != 0 do
    index1 = array.shift
    square_array = array.new.push(index1.to_i**2)
